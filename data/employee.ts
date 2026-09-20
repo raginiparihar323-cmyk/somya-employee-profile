bb6f997c79e2e0ea2df4df333dede8ca7922a597
Ragini Parihar <raginiparihar323@gmail.com>
2026-09-20 17:06:57 +0530

    Make employee profile dynamic

---
+++ data/employee.ts
@@ -0,0 +1,35 @@
+export const employees = {
+  "nistha-jain": {
+    name: "NISTHA JAIN",
+    designation: "INNOVATION EXECUTIVE",
+    photo: "/nistha.jpeg",
+
+    email: "nisthajain609@gmail.com",
+    phone: "8279413939",
+
+    linkedin: "https://www.linkedin.com/in/nistha-jain-577ab0340/",
+    instagram: "https://www.instagram.com/niissssh/",
+
+    employeeId: "SI-IE-1023",
+    role: "Innovation Executive",
+    department: "Innovation & Strategy",
+    location: "Gwalior, India",
+  },
+
+  "somya": {
+    name: "SOMYA",
+    designation: "EMPLOYEE",
+    photo: "/somyain.jpeg",
+
+    email: "somya@example.com",
+    phone: "0000000000",
+
+    linkedin: "",
+    instagram: "",
+
+    employeeId: "SI-EMP-1024",
+    role: "Employee",
+    department: "Innovation & Strategy",
+    location: "Gwalior, India",
+  },
+};
